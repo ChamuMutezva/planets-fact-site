33d9ba24e5802b65b36caa48bb48fcd15383f810
Chamunorwa Mutezva <40114498+ChamuMutezva@users.noreply.github.com>
2021-07-04 15:20:22 +0200

    nav pseudo elements

--- dist/main.d9ee62f6.js
+++ dist/main.d9ee62f6.js
@@ -189,7 +189,7 @@ var reloadCSS = require('_css_loader');
 
 module.hot.dispose(reloadCSS);
 module.hot.accept(reloadCSS);
-},{"./..\\..\\assets\\background-stars.svg":[["background-stars.40a81775.svg","assets/background-stars.svg"],"assets/background-stars.svg"],"_css_loader":"node_modules/parcel-bundler/src/builtins/css-loader.js"}],"node_modules/parcel-bundler/src/builtins/hmr-runtime.js":[function(require,module,exports) {
+},{"./..\\..\\assets\\background-stars.svg":[["background-stars.40a81775.svg","assets/background-stars.svg"],"assets/background-stars.svg"],"./..\\..\\assets\\icon-chevron.svg":[["icon-chevron.96b4f2cf.svg","assets/icon-chevron.svg"],"assets/icon-chevron.svg"],"_css_loader":"node_modules/parcel-bundler/src/builtins/css-loader.js"}],"node_modules/parcel-bundler/src/builtins/hmr-runtime.js":[function(require,module,exports) {
 var global = arguments[3];
 var OVERLAY_ID = '__parcel__error__overlay__';
 var OldModule = module.bundle.Module;
