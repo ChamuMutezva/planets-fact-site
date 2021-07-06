cb10d85c1938affd97bdc44a78257d11d1247dd2
Chamunorwa Mutezva <40114498+ChamuMutezva@users.noreply.github.com>
2021-07-07 00:30:59 +0200

    desktop layout , starter - tabbing section

--- dist/style.967ba849.js
+++ dist/style.967ba849.js
@@ -217,7 +217,7 @@ var parent = module.bundle.parent;
 if ((!parent || !parent.isParcelRequire) && typeof WebSocket !== 'undefined') {
   var hostname = "" || location.hostname;
   var protocol = location.protocol === 'https:' ? 'wss' : 'ws';
-  var ws = new WebSocket(protocol + '://' + hostname + ':' + "54766" + '/');
+  var ws = new WebSocket(protocol + '://' + hostname + ':' + "56383" + '/');
 
   ws.onmessage = function (event) {
     checkedAssets = {};
